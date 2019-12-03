6f3e5f169ce282c922813c87294f851c5a96b478
Tolia35 <anatolia060590@hotmail.com>
2019-12-03 14:23:04 +0100

    Route show checklist OK

--- src/Controller/ChecklistController.php
+++ src/Controller/ChecklistController.php
@@ -1,6 +1,7 @@
 <?php
 namespace App\Controller;
 use App\Entity\Checklist;
+use App\Entity\Item;
 use App\Entity\Checklistitem;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -10,28 +11,14 @@ use Symfony\Component\Routing\Annotation\Route;
 class ChecklistController extends AbstractController
 {
     /**
-     * @Route("/checklist", name="checklist_show", methods={"GET"})
+     * @Route("/checklist/{id}", name="checklist_show", methods={"GET"})
      */
     public function show(Checklist $checklist)
     {
-        $ids = $this->getDoctrine()->getRepository(Id::class)->findBy(['checklist' => $checklist], ['createdAt' => 'DESC']);
+        $items = $this->getDoctrine()->getRepository(Item::class)->findAll();
         return $this->render('checklist/show.html.twig', [
             'checklist' => $checklist,
+            'items' => $items
         ]);
     }
-    /**
-     * @Route("/checklist/new", name="checklist_new")
-     */
-    public function new(Request $request)
-    {
-        $checklist = new Checklist();
-        $form = $this->createForm(ChecklistType::class, $checklist);
-        $form->handleRequest($request);
-        if ($form->isSubmitted() && $form->isValid()) {
-            }
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($checklist);
-            $em->flush();
-            return $this->redirectToRoute("checklist_show");
-        }
 }
