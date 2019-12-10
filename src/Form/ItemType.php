d97f7197f46f8540b4ceaa187c0947abb331de5e
Tolia35 <anatolia060590@hotmail.com>
2019-12-10 14:15:54 +0100

    commit fixe

--- src/Form/ItemType.php
+++ src/Form/ItemType.php
@@ -3,7 +3,10 @@
 namespace App\Form;
 
 use App\Entity\Item;
+use phpDocumentor\Reflection\Type;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,8 +15,8 @@ class ItemType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('reference')
-            ->add('information')
+            ->add('reference',TextType::class, ['label' => 'Référence produit : '])
+            ->add('information', TextType::class,  ['label' => 'Informations produit : '])
         ;
     }
 
